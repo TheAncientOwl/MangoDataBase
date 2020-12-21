e430a8087c41e878764589762ecfbe232ffd4b55
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2020-12-21 19:07:21 +0200

    changed extension h->hpp

---
+++ Mango/Exceptions/MangoExceptions.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "MangoException/MangoException.hpp"
+#include "InvalidSyntaxException/InvalidSyntaxException.hpp"
+#include "InvalidArgumentException/InvalidArgumentException.hpp"
+#include "NanException/NanException.hpp"
+#include "TableAlreadyExistsException/TableAlreadyExistsException.hpp"
+#include "TableNotFoundException/TableNotFoundException.hpp"
+#include "FeatureNotImplementedException/FeatureNotImplementedException.hpp"
