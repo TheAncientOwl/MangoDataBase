29dbf4f54456d724342d0901d648fdd244ebf2d6
Delegeanu Alexandru <68751832+TheAncientOwl@users.noreply.github.com>
2020-12-27 01:14:51 +0200

    Cleaned MangoAPI.hpp includes.

--- Mango/MangoAPI.hpp
+++ Mango/MangoAPI.hpp
@@ -1,7 +1,5 @@
 #pragma once
 #include "MangoDB/MangoDB.hpp"
 #include "Exceptions/MangoExceptions.hpp"
-#include "Row/Row.hpp"
-#include "Row/RowFilters.hpp"
-#include "Queries/Queries.hpp"
 #include "QueryExecutor/QueryExecutor.hpp"
+#include "CommandLineInterface/CommandLineInterface.hpp"
